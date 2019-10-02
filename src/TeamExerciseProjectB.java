9d5ebd744d0cb4db12220914183c2126f36e6b0b
omrez <zwitwitwit@gmail.com>
2019-10-02 12:50:40 +0200

    Version 1

---
+++ src/TeamExerciseProjectB.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class TeamExerciseProjectB {
+
+
+        public static void main(String[] args) {
+
+
+            // Variable Declaration
+            double tip;
+            int satisfied;
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+    }
+
